dbec8eb449c66237e18c4c641a37f524b8b15420
Arunava Basu <arunavabasuDev@gmail.com>
2023-07-03 21:34:28 +0530

    feat: adding assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'movies_app_assignment';
+  searchQuery: string = '';
+  currentPage: number = 1;
+  totalPages: number = 0;
+  movies: any[] = [];
+  isLoading: boolean = false; // Added isLoading property
+
+  constructor(private http: HttpClient) {}
+
+  searchMovies() {
+    const apiKey = 'bc41f730';
+    const url = `http://www.omdbapi.com/?s=${this.searchQuery}&apikey=${apiKey}&page=${this.currentPage}`;
+
+    this.isLoading = true; // Show loader while loading
+
+    this.http.get(url).subscribe(
+      (response: any) => {
+        this.movies = response.Search || [];
+        console.log(this.movies);
+        this.totalPages = Math.ceil(response.totalResults / 10);
+        this.isLoading = false; // Hide loader after loading
+      },
+      (error) => {
+        console.error('Error fetching movies:', error);
+        this.isLoading = false; // Hide loader in case of error
+      }
+    );
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.searchMovies();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.searchMovies();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MovieCardComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
